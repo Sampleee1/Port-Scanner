caff75141929143d49943e623ea0c588eda0eded
Sampleee1 <fred.gamer.br1@gmail.com>
2024-10-08 23:07:02 -0300

    Design improvement and fix some bugs

--- portscanner.py
+++ portscanner.py
@@ -1,36 +1,64 @@
 import socket
 from datetime import datetime
+import threading 
 
-target = input("Digite o IP ou dominio que voce quer escanear: ")
-
-target_ip = socket.gethostbyname(target)
+i = 0
 
 
-print(f"Escaneando {target_ip}")
-print("-" * 50)
-start_time = datetime.now()
-
-try:
-    for port in range(1, 1025):
+def scan_port(target_ip, port):
+    global i
+    try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = sock.connect_ex((target_ip, port))
 
         if result == 0:
-            print(f"Porta {port} esta aberta!")
-        sock.close()
+            print(f"Porta {port} está aberta!")
+            i += 1
+        sock.close
 
-except KeyboardInterrupt:
-    print("processo interrompido")
-    exit()
+    except KeyboardInterrupt:
+        print("processo interrompido")
+        exit()
 
-except socket.gaierror:
-    print("\nHostname não pode ser resolvido")
-    exit()
+    except socket.gaierror:
+        print("\nHostname não pode ser resolvido")
+        exit()
 
-except socket.error:
-    print("\nErro ao conectar ao servidor")
-    exit()
+    except socket.error:
+        print("\nErro ao conectar ao servidor")
+        exit()
+
+def start_scan(target_ip, port_range):
+    threads = []
+
+    for port in range(1, 1025):
+        thread = threading.Thread(target=scan_port, args=(target_ip, port))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+target = input("Digite o IP ou dominio que voce quer escanear: ")
+
+target_ip = socket.gethostbyname(target)
+
+print("\n" * 100)
+print(f"Escaneando {target} ({target_ip})")
+start_time = datetime.now()
+tempoI = start_time.strftime('%H : %M : %S')
+print(f"Horario de inicio: {tempoI}")
+print("-" * 50)
+
+start_scan(target_ip, range(1, 1025))
+
+if i == 0: 
+    print("Nenhuma porta esta aberta!")
 
 end_time = datetime.now()
-print(f"Tempo gasto no escaneamento: {end_time - start_time}")
+tempoF = end_time.strftime('%H : %M : %S')
+print("-" * 50)
+print(f"Horario final: {tempoF}")
+print(f"Tempo gasto no escaneamento: {(end_time - start_time)}")
